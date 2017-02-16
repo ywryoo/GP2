a81d655d8c1bfc2cccd69582c35d6c3a92879c01
Yangwook Ryoo <ywryoo@gmail.com>
2017-02-17 01:19:13 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='Graduation Project 2',
     long_description=__doc__,
     version='0.1.0',
-    packages=['gp2-core'],
+    # packages=['gp2-core'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
