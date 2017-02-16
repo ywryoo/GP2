0d48dbd07f5ed8468561e6b4098e42134e294006
Yangwook Ryoo <ywryoo@gmail.com>
2017-02-17 03:06:22 +0900

    add gp2gatherplot

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ MIT License: see LICENSE at root directory
 from flask import Flask
 from gp2gatherplot.gatherplot import gatherplot
 
-app = Flask('gp-core')
+app = Flask(__name__)
 app.register_blueprint(gatherplot, url_prefix='/gatherplot')
 
 
 @app.route('/')
 def route_root():
-    return 'Modules are Here!<br><a href="./gatherpot">gatherplot</a>'
+    return 'Modules are Here!<br><a href="./gatherplot">gatherplot</a>'
